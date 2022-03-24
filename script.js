d96ab371e5357e8981facbdbc429fc9985a673c7
AGESILAS Ludovic <98770184+ldc45@users.noreply.github.com>
2022-03-24 20:31:32 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const message =
+  "Thank you for submitting your request. The crew will respond shortly.";
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+  });
